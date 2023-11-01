d6e9ea9ac401f39ca52798510d4798fa54a9d908
vihmavari <vihmavari.03@gmail.com>
2023-11-01 16:30:17 +0300

    initialize two arrays to start game, add allignment by center

--- script.js
+++ script.js
@@ -5,14 +5,14 @@
 	var Memory = {
 
 		// создаём карточку
-		init: function(cards){
+		init: function(cards1, cards2){
 			//  получаем доступ к классам
 			this.$game = $(".game");
 			this.$modal = $(".modal");
 			this.$overlay = $(".modal-overlay");
 			this.$restartButton = $("button.restart");
 			// собираем из карточек массив — игровое поле
-			this.cardsArray = $.merge(cards, cards);
+			this.cardsArray = $.merge(cards1, cards2);
 			// перемешиваем карточки
 			this.shuffleCards(this.cardsArray);
 			// и раскладываем их
@@ -158,7 +158,73 @@
 	};
 	
 	// карточки
-	var cards = [
+	var cards1 = [
+		{	
+			// название
+			name: "php",
+			// адрес картинки
+			img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/297px-PHP-logo.svg.png",
+			// порядковый номер пары
+			id: 1,
+		},
+		{
+			name: "css3",
+			img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/160px-CSS3_logo.svg.png",
+			id: 2
+		},
+		{
+			name: "html5",
+			img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/160px-HTML5_logo_and_wordmark.svg.png",
+			id: 3
+		},
+		{
+			name: "jquery",
+			img: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/JQuery-Logo.svg/440px-JQuery-Logo.svg.png",
+			id: 4
+		}, 
+		{
+			name: "javascript",
+			img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/160px-Unofficial_JavaScript_logo_2.svg.png",
+			id: 5
+		},
+		{
+			name: "node",
+			img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/262px-Node.js_logo.svg.png",
+			id: 6
+		},
+		{
+			name: "photoshop",
+			img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Adobe_Photoshop_CC_icon.svg/164px-Adobe_Photoshop_CC_icon.svg.png",
+			id: 7
+		},
+		{
+			name: "python",
+			img: "https://www.python.org/static/img/python-logo@2x.png",
+			id: 8
+		},
+		{
+			name: "rails",
+			img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Ruby_On_Rails_Logo.svg/425px-Ruby_On_Rails_Logo.svg.png",
+			id: 9
+		},
+		{
+			name: "sass",
+			img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/213px-Sass_Logo_Color.svg.png",
+			id: 10
+		},
+		{
+			name: "sublime",
+			img: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Breezeicons-apps-48-sublime-text.svg/160px-Breezeicons-apps-48-sublime-text.svg.png",
+			id: 11
+		},
+		{
+			name: "wordpress",
+			img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/WordPress_logo.svg/440px-WordPress_logo.svg.png",
+			id: 12
+		},
+	];
+	
+	var cards2 = [
 		{	
 			// название
 			name: "php",
@@ -225,7 +291,7 @@
 	];
 	
 	// запускаем игру
-	Memory.init(cards);
+	Memory.init(cards1, cards2);
 })();
 
 
