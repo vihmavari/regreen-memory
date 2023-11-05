204ead3a740714a31e5d0bf6da0629371c824f10
vihmavari <vihmavari.03@gmail.com>
2023-11-05 21:49:09 +0300

    return img-blovk, but without img

--- script.js
+++ script.js
@@ -149,7 +149,8 @@
 				frag += '<div class="card" data-id="'+ v.id +'"><div class="inside">\
 				<div class="front"><img src="'+ v.img +'"\
 				alt="'+ v.name +'" /></div>\
-				<div class="back"></div></div>\
+				<div class="back"><img src=""\
+				alt="hiii :3" /></div></div>\
 				</div>';
 			});
 			// возвращаем собранный код
